1b372bdf3a979de5b9e6006812158400a15ded73
FernandoBJ32 <fbadillaj@gmail.com>
2022-08-23 20:13:29 -0600

    todo por el momento

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
+
 import Button from '../button/button.component';
 
 import CartItem from '../cart-item/cart-item.component';
@@ -12,6 +13,8 @@ import {CartDropdownContainer,EmptyMessage, CartItems} from './cart-dropdown.sty
 
 const CartDropdown = () => {
 const {cartItems} = useContext (CartContext);
+
+
 const navigate = useNavigate();
 
 const goToCheckoutHandler = () => {
@@ -22,14 +25,14 @@ const goToCheckoutHandler = () => {
     <CartDropdownContainer>
       <CartItems>
         {
-          cartItems.length ? (cartItems.map((item) =>( 
+          cartItems.length ? (cartItems.map((item) =>
           <CartItem key={item.di} cartItem={item} />
-          ))) : (
+          )) : (
             <EmptyMessage> Your cart is empty </EmptyMessage>
           )
         }
       </CartItems>
-      <Button onClick= {goToCheckoutHandler}> TO CHECKOUT </Button>
+      <Button onClick= {goToCheckoutHandler}>GO TO CHECKOUT </Button>
     </CartDropdownContainer>
   )
 }
--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from 'react';
+
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
-// import SHOP_DATA from '../shop-data';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
@@ -8,16 +8,15 @@ export const CategoriesContext = createContext({
 
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
-  
-  useEffect (() => {
+
+  useEffect(() => {
     const getCategoriesMap = async () => {
-    const categoryMap = await getCategoriesAndDocuments();
-    setCategoriesMap(categoryMap);
-    }
+      const categoryMap = await getCategoriesAndDocuments('categories');
+      setCategoriesMap(categoryMap);
+    };
+
     getCategoriesMap();
-
   }, []);
-
 
   const value = { categoriesMap };
   return (
